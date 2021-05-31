db3297e75b1f141ae2a306b277223099d718624e
yolilufei <15201507672@163.com>
2021-05-31 17:27:53 +0800

    初始化nuxt项目

---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>about</div>
+</template>
+<script>
+export default {
+    name: 'About'
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Hello Next.js</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'HomePage'
+}
+</script>
